57ea8cdaee0ebc0b0c52f5990de92b24ae5dd59d
Adam Watkins <adam@stupidpupil.co.uk>
2021-06-20 14:31:19 +0100

    Add the ability to produce a map of the GTFS stops, and add an example of this

---
+++ R/pretty_wales_ish_map.R
@@ -0,0 +1,15 @@
+pretty_wales_ish_map <- function(){
+
+  stops <- tibble()
+
+  for(fn in dir("output", ".gtfs.zip")){
+    gtfs <- better_gtfs_read(paste0("output/", fn))
+
+    stops <- stops %>% bind_rows(
+      gtfs$stops %>% select(stop_id, stop_lat, stop_lon) %>%
+      mutate(filename= fn)
+    )
+  }
+
+  plot_map <- ggplot(stops, aes(x=stop_lon, y=stop_lat, colour=filename)) + geom_point(size=0.5, alpha=0.5) + theme_minimal() + theme(legend.position = 'none', panel.grid = element_blank(), axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank())
+}
