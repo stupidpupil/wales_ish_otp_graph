400cf96d1e4a9891599de70993ce27836531cded
Adam Watkins <adam@stupidpupil.co.uk>
2021-06-28 00:27:30 +0100

    Update download_atoc to use latest rvest functions

--- R/download_atoc.R
+++ R/download_atoc.R
@@ -1,17 +1,17 @@
 download_atoc <- function(){
 
-  atoc_session <- rvest::html_session("http://data.atoc.org/user/login")
+  atoc_session <- rvest::session("http://data.atoc.org/user/login")
 
   atoc_login_form <- atoc_session %>% rvest::html_node("#user-login") %>% rvest::html_form() %>%
-    rvest::set_values(name=config::get()$atoc_username, pass=config::get()$atoc_password)
+    rvest::html_form_set(name=config::get()$atoc_username, pass=config::get()$atoc_password)
 
-  atoc_session <- atoc_session %>% rvest::submit_form(atoc_login_form)
+  atoc_session <- atoc_session %>% rvest::session_submit(atoc_login_form)
 
-  atoc_session <- atoc_session %>% rvest::jump_to("http://data.atoc.org/data-download")
+  atoc_session <- atoc_session %>% rvest::session_jump_to("http://data.atoc.org/data-download")
 
   atoc_download_url <- atoc_session %>% rvest::html_node("#field_timetable_file-wrapper a") %>% rvest::html_attr("href")
 
-  atoc_session <- atoc_session %>% rvest::jump_to(atoc_download_url)
+  atoc_session <- atoc_session %>% rvest::session_jump_to(atoc_download_url)
 
   writeBin(atoc_session$response$content, "data-raw/atoc.zip") 
 
