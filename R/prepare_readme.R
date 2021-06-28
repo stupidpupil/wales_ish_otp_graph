f1e0b23f1074b5657ffe489983ffe8d4462a6395
Adam Watkins <adam@stupidpupil.co.uk>
2021-06-28 16:26:31 +0100

    Produce GFMarkdown version of graph readme

--- R/prepare_readme.R
+++ R/prepare_readme.R
@@ -1,3 +1,3 @@
 prepare_readme <- function(){
-  rmarkdown::render("data-raw/output_readme.Rmd", knit_root_dir="..", output_dir="output")
+  rmarkdown::render("data-raw/output_readme.Rmd", knit_root_dir="..", output_dir="output", output_format="all")
 }
