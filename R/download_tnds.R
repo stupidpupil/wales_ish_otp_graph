fc48441c7d2fd12f61cfd2200200aed5052c92cb
Adam Watkins <adam@stupidpupil.co.uk>
2021-06-29 23:25:01 +0100

    Add National Coach Services (NCSD)

--- R/download_tnds.R
+++ R/download_tnds.R
@@ -2,4 +2,8 @@ download_tnds <- function(){
   download.file(
     paste0("ftp://",config::get()$tnds_username,":",config::get()$tnds_password,"@ftp.tnds.basemap.co.uk/W.zip"),
      "data-raw/wales.bus.tnds.zip")
+
+  download.file(
+    paste0("ftp://",config::get()$tnds_username,":",config::get()$tnds_password,"@ftp.tnds.basemap.co.uk/NCSD.zip"),
+     "data-raw/ncsd.bus.tnds.zip")
 }
--- R/prepare_tnds_gtfs.R
+++ R/prepare_tnds_gtfs.R
@@ -1,6 +1,22 @@
 prepare_tnds_gtfs <- function(){
-  gtfs <- transxchange2gtfs(path_in = "data-raw/wales.bus.tnds.zip", ncores= (parallel::detectCores()-1), try_mode=TRUE)
+  gtfs <- transxchange2gtfs(
+    path_in = "data-raw/wales.bus.tnds.zip", ncores= (parallel::detectCores()-1), 
+    try_mode=TRUE, scotland="no")
   gtfs <- gtfs %>% gtfs_wales_ish_ify()
   gtfs %>% gtfs_write(folder="output", name="wales.bus.walesish.gtfs")
+
+
+
+  # NCSD TXC stuff is buried within the zip
+  unlink("data-raw/ncsd.bus.tnds", recursive=TRUE)
+  unzip("data-raw/ncsd.bus.tnds.zip", exdir="data-raw/ncsd.bus.tnds")
+  ncsd_files <- list.files("data-raw/ncsd.bus.tnds/NCSD_TXC/", pattern="*.xml", full.names=TRUE)
+  gtfs <- transxchange2gtfs(
+    path_in = ncsd_files, ncores= (parallel::detectCores()-1), 
+    try_mode=TRUE, scotland="no")
+
+  gtfs <- gtfs %>% gtfs_wales_ish_ify()
+  gtfs %>% gtfs_write(folder="output", name="ncsd.bus.walesish.gtfs")
+  unlink("data-raw/ncsd.bus.tnds", recursive=TRUE)
 }
 
