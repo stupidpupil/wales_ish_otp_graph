25ce60ac272c92e8f6f0fe98d72bbb2fbdfe9aa9
Adam Watkins <adam@stupidpupil.co.uk>
2021-07-03 15:34:19 +0100

    Generate .meta.json files for graphs

--- R/prepare_street_graph.R
+++ R/prepare_street_graph.R
@@ -1,3 +1,16 @@
 prepare_street_graph <- function(){
-  system(paste0(java_command(), " -jar data-raw/otp.jar --buildStreet output"))
+
+  cmd <- paste0(java_command(), " -jar data-raw/otp.jar --buildStreet output")
+
+  system(cmd)
+
+  list(
+    CreatedAt = now() %>% format_ISO8601(usetz=TRUE),
+    CreatedWithCommand = cmd,
+    CreatedWithOpenTripPlannerVersion = otp_version(),
+    MaxSpatialExtent = wales_ish_bounding_box_string,
+    DerivedFrom = I(describe_file("output/wales_ish.osm.pbf"))
+  ) %>% toJSON(pretty = TRUE) %>%
+  write("output/streetGraph.obj.meta.json")
+
 }
--- R/prepare_transport_graph.R
+++ R/prepare_transport_graph.R
@@ -1,3 +1,16 @@
 prepare_transport_graph <- function(){
-  system(paste0(java_command(), " -jar data-raw/otp.jar --loadStreet --save output"))
+
+  cmd <- paste0(java_command(), " -jar data-raw/otp.jar --loadStreet --save output")
+  
+  system(cmd)
+
+  list(
+    CreatedAt = now() %>% format_ISO8601(usetz=TRUE),
+    CreatedWithCommand = cmd,
+    CreatedWithOpenTripPlannerVersion = otp_version(),
+    MaxSpatialExtent = wales_ish_bounding_box_string,
+    DerivedFrom = list.files("output/", "(streetGraph.obj|\\.gtfs\\.zip)$", full.names=TRUE) %>% sapply(describe_file, USE.NAMES=F)
+  ) %>% toJSON(pretty = TRUE) %>%
+  write("output/graph.obj.meta.json")
+
 }
