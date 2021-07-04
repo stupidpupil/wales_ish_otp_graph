056a7b6a7b6e0aee2f4d9bb25fa31f7c9b9052a0
Adam Watkins <adam@stupidpupil.co.uk>
2021-07-04 23:58:11 +0100

    Split release body from the README/GitHub page

--- R/prepare_readme.R
+++ R/prepare_readme.R
@@ -1,3 +1,4 @@
 prepare_readme <- function(){
   rmarkdown::render("data-raw/output_readme.Rmd", knit_root_dir="..", output_dir="output", output_format="all")
+  rmarkdown::render("data-raw/release_body.Rmd", knit_root_dir="..", output_dir="output", output_format="all")
 }
