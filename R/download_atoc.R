cbe0c17bcedae22007cfe0aa796957a52b5559b8
Adam Watkins <adam@stupidpupil.co.uk>
2021-07-11 14:44:33 +0100

    Try to make ATOC download more reliable

--- R/download_atoc.R
+++ R/download_atoc.R
@@ -1,5 +1,9 @@
+atoc_user_agent <- function(){
+  httr::user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:89.0) Gecko/20100101 Firefox/89.0")
+}
+
 get_logged_in_atoc_session <- function(){
-  atoc_session <- rvest::session("http://data.atoc.org/user/login")
+  atoc_session <- rvest::session("http://data.atoc.org/user/login", atoc_user_agent())
 
   atoc_login_form <- atoc_session %>% rvest::html_node("#user-login") %>% rvest::html_form() %>%
     rvest::html_form_set(name=config::get()$atoc_username, pass=config::get()$atoc_password)
@@ -8,7 +12,6 @@ get_logged_in_atoc_session <- function(){
 
  return(atoc_session) 
 }
-
 
 get_atoc_download_url <- function(){
 
@@ -21,15 +24,25 @@ get_atoc_download_url <- function(){
   return(atoc_download_url)
 }
 
-download_atoc <- function(){
+download_atoc <- function(retries=3L){
 
   atoc_download_url <- get_atoc_download_url()
   atoc_session <- get_logged_in_atoc_session()
 
-  atoc_session <- atoc_session %>% rvest::session_jump_to("http://data.atoc.org/data-download")
-  atoc_session <- atoc_session %>% rvest::session_jump_to(atoc_download_url)
+  #atoc_session <- atoc_session %>% rvest::session_jump_to("http://data.atoc.org/data-download")
+  atoc_session <- atoc_session %>% rvest::session_jump_to(atoc_download_url, atoc_user_agent())
 
-  print(atoc_session)
+  actual_content_length <- atoc_session$response$content %>% length()
+  claimed_content_length <- as.integer(atoc_session$response$headers$`content-length`)
+
+  if(actual_content_length == 0L | actual_content_length < claimed_content_length){
+    print("ATOC zip was empty or didn't match content-length header. Retrying in 60 seconds…")
+    Sys.sleep(60L)
+    if(retries > 0){
+      download_atoc(retries-1L)
+    }
+    return()
+  }
 
   writeBin(atoc_session$response$content, "data-raw/atoc.zip")
 
