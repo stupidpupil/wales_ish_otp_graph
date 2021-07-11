b36a75cdcccfdc99dc0c43892bcd2766ce59bee7
Adam Watkins <adam@stupidpupil.co.uk>
2021-07-11 19:07:34 +0100

    Perform some very basic checks on the outcome of test journeys

---
+++ R/check_test_journeys.R
@@ -0,0 +1,51 @@
+check_test_journeys <- function(){
+
+  tests <- tibble(Description = character(0), Passed = logical(0))
+
+  test_journeys <- read_csv("output/test_journeys.csv")
+
+
+  next_test <- tibble(
+    Description = "Cardiff to Sheffield can't be routed",
+    Passed = TRUE
+  )
+
+  if(!(test_journeys %>% filter(Description == "Cardiff to Sheffield") %>% pull(durationSeconds) %>% is.na() %>% all())){
+    next_test$Passed = FALSE
+  }
+
+  tests <- tests %>% bind_rows(next_test)
+
+  next_test <- tibble(
+    Description = "All journeys within Walesish can be routed",
+    Passed = TRUE
+  )
+
+  if(test_journeys %>% filter(Description != "Cardiff to Sheffield") %>% pull(durationSeconds) %>% is.na() %>% any()){
+    next_test$Passed = FALSE
+  }
+
+  tests <- tests %>% bind_rows(next_test)
+
+  next_test <- tibble(
+    Description = "Cardiff to Bala and its reverse take roughly the same amount of time to drive",
+    Passed = TRUE
+  )
+
+  ctob_duration <- test_journeys %>% filter(Description == "Cardiff to Bala", !public) %>% pull(durationSeconds)
+  btoc_duration <- test_journeys %>% filter(Description == "Bala to Cardiff", !public) %>% pull(durationSeconds)
+
+  if(ctob_duration > 1.05*btoc_duration | btoc_duration > 1.05*ctob_duration){
+    next_test$Passed = FALSE
+  }
+
+  tests <- tests %>% bind_rows(next_test)
+
+  if(any(!tests$Passed)){
+    cat(tests %>% filter(!Passed) %>% pull(Description) %>% paste0(collapse="\n"))
+    stop("Some checks on test journeys failed!")
+  }
+
+  print("All checks on test journeys passed!")
+
+}
