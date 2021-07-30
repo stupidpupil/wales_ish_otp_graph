08924f999831121fd58a9d71eb7b0837268d013a
Adam Watkins <adam@stupidpupil.co.uk>
2021-07-30 15:10:10 +0100

    Crude attempts to use JDK 11

--- data-raw/run-otp.bat
+++ data-raw/run-otp.bat
@@ -1,1 +1,5 @@
+if exist "C:\Progra~1\AdoptOpenJDK\jdk-11.0.11.9-hotspot\release" (
+  set JAVA_HOME "C:\Progra~1\AdoptOpenJDK\jdk-11.0.11.9-hotspot"
+)
+
 java --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED  -Xmx8g -jar otp.jar --load graph
--- data-raw/run-otp.sh
+++ data-raw/run-otp.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+if command -v /usr/libexec/java_home 2>&1 /dev/null
+then
+  echo "Trying to set JAVA_HOME using /usr/libexec/java_home"
+  JAVA_HOME="$(/usr/libexec/java_home -v 11)"
+  export JAVA_HOME
+fi
+
 java --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED -Xmx8g -jar otp.jar --load graph
