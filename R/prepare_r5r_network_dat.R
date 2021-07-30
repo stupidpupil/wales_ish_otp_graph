35d35e7620ddb2ca8628a0020ba98386db4933bb
Adam Watkins <adam@stupidpupil.co.uk>
2021-07-30 11:57:12 +0100

    Move r5r prep code to R, create meta.json and include as a release asset

---
+++ R/prepare_r5r_network_dat.R
@@ -0,0 +1,19 @@
+prepare_r5r_network_dat <- function(){
+  options(java.parameters = java_args())
+
+  # HACK
+  if(!require('r5r', character.only = TRUE)){
+    install.packages('r5r', dependencies = TRUE)
+    library('r5r', character.only = TRUE)
+  }
+
+  setup_r5(data_path="output")
+
+  list(
+    CreatedAt = now() %>% format_ISO8601(usetz=TRUE),
+    MaxSpatialExtent = wales_ish_bounding_box_string,
+    DerivedFrom = list.files("output/", "(\\.osm.pbf|\\.gtfs\\.zip)$", full.names=TRUE) %>% sapply(describe_file, USE.NAMES=F)
+  ) %>% toJSON(pretty = TRUE) %>%
+  write("output/network.dat.meta.json")
+
+}
