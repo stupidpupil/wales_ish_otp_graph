1f97449271078586fe94a64121a5123776e13636
Adam Watkins <adam@stupidpupil.co.uk>
2021-07-30 15:36:32 +0100

    Try to be helpful in the event of Java/OTP crashing etc.

--- data-raw/run-otp.bat
+++ data-raw/run-otp.bat
@@ -1,5 +1,22 @@
+@echo off
+
 if exist "C:\Progra~1\AdoptOpenJDK\jdk-11.0.11.9-hotspot\release" (
   set JAVA_HOME "C:\Progra~1\AdoptOpenJDK\jdk-11.0.11.9-hotspot"
 )
 
+PATH %PATH%;%JAVA_HOME%\bin\
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "JAVA_MAJOR_VERSION=%%j%%k%%l%%m"
+
+if %JAVA_MAJOR_VERSION% LSS 11 (
+  echo It looks like you need a newer version of Java.
+  echo Try installing JDK11 from https://adoptopenjdk.net/ !
+  pause
+  exit
+)
+
 java --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED  -Xmx8g -jar otp.jar --load graph
+
+if %ERRORLEVEL% GEQ 1 (
+  pause
+  exit
+)
--- data-raw/run-otp.sh
+++ data-raw/run-otp.sh
@@ -7,4 +7,22 @@ then
   export JAVA_HOME
 fi
 
+JAVA_MAJOR_VERSION="$(java -fullversion 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)"
+
+if [ "$JAVA_MAJOR_VERSION" -lt 11 ]
+then
+  echo "It looks like you need a newer version of Java."
+  echo "Try installing JDK11 from https://adoptopenjdk.net/ !"
+  echo "Press the [Enter] key to continue…"
+  read
+  exit
+fi
+
 java --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED -Xmx8g -jar otp.jar --load graph
+
+if [ $? -gt 0 ]
+then
+  echo "Press the [Enter] key to continue…"
+  read
+  exit
+fi
