41751095f6b57532a980df8a184c3a082166b317
Adam Watkins <adam@stupidpupil.co.uk>
2022-01-10 21:32:37 +0000

    Fix typo

--- R/prepare_osm_config.R
+++ R/prepare_osm_config.R
@@ -2,4 +2,3 @@ prepare_osm_config <- function(){
   file.copy(dir_support("osm_config/build-config.json"), dir_output("build-config.json"), overwrite=TRUE)
   file.copy(dir_support("osm_config/router-config.json"), dir_output("router-config.json"), overwrite=TRUE)
 }
--
