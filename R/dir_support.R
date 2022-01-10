8b8b66d4ebcbecb5af868f27ccd8795b8e046731
Adam Watkins <adam@stupidpupil.co.uk>
2022-01-10 23:42:31 +0000

    Move dir_support to inst/exdata

--- R/dir_support.R
+++ R/dir_support.R
@@ -2,7 +2,7 @@ dir_support <- function(fn){
   ret <-config::get()$dir_support
 
   if(is.null(ret)){
-    ret <- system.file("data-raw", package=package_name())
+    ret <- system.file("extdata", package=package_name())
   }
 
   if(!missing(fn)){
