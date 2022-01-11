51048221787fc9d9a1c7ff45e1359f1803f3148e
Adam Watkins <adam@stupidpupil.co.uk>
2022-01-11 11:11:20 +0000

    Actually include the otp-config files...

--- R/prepare_otp_config.R
+++ R/prepare_otp_config.R
@@ -1,4 +1,9 @@
 prepare_otp_config <- function(){
   file.copy(dir_support("otp_config/build-config.json"), dir_output("build-config.json"), overwrite=TRUE)
   file.copy(dir_support("otp_config/router-config.json"), dir_output("router-config.json"), overwrite=TRUE)
+
+  stopifnot(file.exists(dir_output("build-config.json")))
+  stopifnot(file.exists(dir_output("router-config.json")))
+
+  TRUE
 }
