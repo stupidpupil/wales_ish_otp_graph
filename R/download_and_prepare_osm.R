4920704e5379b3620a22e3f418078f9dd30abcee
Adam Watkins <adam@stupidpupil.co.uk>
2022-01-11 00:13:46 +0000

    Force a 10-min timeout when download...osm

--- R/download_and_prepare_osm.R
+++ R/download_and_prepare_osm.R
@@ -1,4 +1,6 @@
 download_and_prepare_osm <- function(){
+  old_opts <- options(timeout=600)
+  on.exit(options(old_opts))
 
   gb_osm_url <- "https://download.geofabrik.de/europe/great-britain-latest.osm.pbf"
   dest_path <- dir_working("great-britain-latest.osm.pbf")
