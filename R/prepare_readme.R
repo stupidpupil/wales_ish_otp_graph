be709e1fa51d80cfef1002d565a9cfcad4f75d31
Adam Watkins <adam@stupidpupil.co.uk>
2022-01-11 12:58:11 +0000

    Fix prepare_readme working directory

--- R/prepare_readme.R
+++ R/prepare_readme.R
@@ -1,4 +1,4 @@
 prepare_readme <- function(){
-  rmarkdown::render(dir_support("output_readme.Rmd"), knit_root_dir="..", output_dir=dir_output(), output_format="all")
-  rmarkdown::render(dir_support("release_body.Rmd"),  knit_root_dir="..", output_dir=dir_output(), output_format="all")
+  rmarkdown::render(dir_support("output_readme.Rmd"), knit_root_dir=normalizePath("."), output_dir=dir_output(), output_format="all")
+  rmarkdown::render(dir_support("release_body.Rmd"),  knit_root_dir=normalizePath("."), output_dir=dir_output(), output_format="all")
 }
