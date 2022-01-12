e3b912fec1398e640c659398e21e8f4c4943cee0
Adam Watkins <adam@stupidpupil.co.uk>
2022-01-12 00:14:16 +0000

    Add force_merge to transxchange2gtfs

--- R/prepare_tnds_gtfs.R
+++ R/prepare_tnds_gtfs.R
@@ -9,7 +9,7 @@ prepare_tnds_gtfs <- function(){
 
     gtfs <- UK2GTFS::transxchange2gtfs(
       path_in = src_path, ncores= (parallel::detectCores()-1), 
-      try_mode=TRUE, scotland=ifelse(r == "S", "yes", "no"))
+      try_mode=TRUE, scotland=ifelse(r == "S", "yes", "no"), force_merge = TRUE)
     gtfs <- gtfs %>% gtfs_wales_ish_ify()
     gtfs %>% UK2GTFS::gtfs_write(folder=dir_output(), name=paste0(r,".tnds.", output_affix(), ".gtfs"))
 
@@ -27,14 +27,15 @@ prepare_tnds_gtfs <- function(){
   ncsd_files <- list.files(dir_working("NCSD.bus.tnds/NCSD_TXC/"), pattern="*.xml", full.names=TRUE)
   gtfs <- UK2GTFS::transxchange2gtfs(
     path_in = ncsd_files, ncores= (parallel::detectCores()-1), 
-    try_mode=TRUE, scotland="no")
+    try_mode=TRUE, scotland="no", force_merge = TRUE)
 
   gtfs <- gtfs %>% gtfs_wales_ish_ify()
 
   if(gtfs$routes %>% nrow() > 0){
-    gtfs$routes$route_type = 202
+    gtfs$routes$route_type <- 202 
+    # "National Coach Service" per https://developers.google.com/transit/gtfs/reference/extended-route-types
   }
-  
+
   gtfs %>% UK2GTFS::gtfs_write(folder=dir_output(), name=paste0("NCSD.tnds.", output_affix(), ".gtfs"))
   unlink(dir_working("NCSD.bus.tnds"), recursive=TRUE)
 
