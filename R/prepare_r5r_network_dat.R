9e93e7525edb6b3c6347d69905117ba544b67120
Adam Watkins <adam@stupidpupil.co.uk>
2022-04-30 21:21:44 +0100

    Add r5r and R5 version to network.dat.meta.json

--- R/prepare_r5r_network_dat.R
+++ R/prepare_r5r_network_dat.R
@@ -13,6 +13,8 @@ prepare_r5r_network_dat <- function(){
 
   list(
     CreatedAt = now_as_iso8601(),
+    CreatedWithR5rVersion = packageVersion("r5r"),
+    CreatedWithR5Version = formals(r5r::setup_r5)$version,
     DerivedFrom = list.files(dir_output(), "(\\.osm.pbf|\\.gtfs\\.zip)$", full.names=TRUE) %>% lapply(describe_file)
   ) %>% jsonlite::toJSON(pretty = TRUE) %>%
   write(dir_output("network.dat.meta.json"))
