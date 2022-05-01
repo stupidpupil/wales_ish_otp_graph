eceb2e4550a94efaf15a53c566fcb13932261f47
Adam Watkins <adam@stupidpupil.co.uk>
2022-05-01 19:13:29 +0100

    Add pfaedle_all_output_gtfs()

--- R/pfaedle_a_gtfs_zip.R
+++ R/pfaedle_a_gtfs_zip.R
@@ -1,3 +1,9 @@
+pfaedle_all_output_gtfs <- function(){
+  for(fn in list.files(dir_output(), "\\.gtfs\\.zip$", full.names=TRUE)){
+    pfaedle_a_gtfs_zip(fn)
+  }
+}
+
 pfaedle_a_gtfs_zip <- function(path_to_gtfs_zip, path_to_osm = dir_output(paste0(output_affix(), ".osm.pbf"))){
 
   # Unzip gtfs zip to a temporary folder in dir working
