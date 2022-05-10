e72f175a0073fba8b36b5881dd7fd1a2cfe843c3
Adam Watkins <adam@stupidpupil.co.uk>
2022-05-10 14:08:15 +0100

    Allow dir_support() to work without a config.yml

--- R/dir_support.R
+++ R/dir_support.R
@@ -1,5 +1,10 @@
 dir_support <- function(fn){
-  ret <-config::get()$dir_support
+
+  ret <- NULL
+
+  try({
+    ret <- config::get()$dir_support
+  }, silent=TRUE)
 
   if(is.null(ret)){
     ret <- system.file("extdata", package=package_name())
