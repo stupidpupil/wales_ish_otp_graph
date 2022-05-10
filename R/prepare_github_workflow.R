4f1df0c4f24979968aad6fe17dc3e3720e4e6a0a
Adam Watkins <adam@stupidpupil.co.uk>
2022-05-10 14:13:33 +0100

    Fix name and (for now) manually unquote 'on'

--- R/prepare_github_workflow.R
+++ R/prepare_github_workflow.R
@@ -58,5 +58,5 @@ prepare_github_workflow <- function() {
 
   ret %>%
     yaml::as.yaml() %>%
-    writeChar(".github/workflows/build_and_release.yaml")
+    writeChar(".github/workflows/build-and-release.yaml")
 }
