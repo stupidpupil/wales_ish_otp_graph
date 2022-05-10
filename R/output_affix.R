16fb0221cfaee286ddb1cd0f8e6b6997feaa28b6
Adam Watkins <adam@stupidpupil.co.uk>
2022-05-10 14:43:13 +0100

    Fix output_affix check

--- R/output_affix.R
+++ R/output_affix.R
@@ -1,8 +1,10 @@
 output_affix <- function() {
   ret <- config::get()$output_affix
 
-  if(!is.null(ret) & ret != ""){
-    return(ret)
+  if(!is.null(ret)){
+    if(ret != ""){
+      return(ret)      
+    }
   }
 
   "walesish"
