eff0fc02d6c956065b4d916c0ea6c3697df626fa
Adam Watkins <adam@stupidpupil.co.uk>
2022-05-12 17:35:04 +0100

    Include entire rail network when preparing OSM

--- R/download_and_prepare_osm.R
+++ R/download_and_prepare_osm.R
@@ -17,25 +17,57 @@ download_osm <- function(){
 
 prepare_osm <- function(){
   src_path <- dir_working("great-britain-latest.osm.pbf")
+  dest_path <- dir_output(output_affix(), ".osm.pbf")
+
+  unlink(dest_path)
  
   prepare_bounds_geojson()
 
-  osmium_command = paste0(
+  bounded_temp_path <- tempfile(tmpdir = dir_working(), fileext=".osm.pbf")
+
+  bounded_osmium_command <- paste0(
     "osmium extract -p ",
     path_to_bounds_geojson(),
     " -s smart ",
     src_path,
-    " -o ", 
-    dir_output(output_affix(), ".osm.pbf")
+    " -o ", bounded_temp_path
   )
 
-  system(osmium_command)
+  system(bounded_osmium_command)
+  stopifnot(file.exists(bounded_temp_path))
+
+  rail_temp_path <- tempfile(tmpdir = dir_working(), fileext=".osm.pbf")
+
+  rail_osmium_command <- paste0(
+    "osmium tags-filter ",
+    "--expressions ", dir_support("rail_osm_tags.txt"),
+    " ", src_path,
+    " -o ", rail_temp_path
+  )
+
+  system(rail_osmium_command)
+  stopifnot(file.exists(rail_temp_path))
+
+
+  merge_osmium_command <- paste0(
+    "osmium merge",
+    " ", bounded_temp_path,
+    " ", rail_temp_path,
+    " -o ", dest_path
+  )
+
+  system(merge_osmium_command)
+
+  stopifnot(file.exists(dest_path))
+
+  unlink(bounded_temp_path)
+  unlink(rail_temp_path)
 
   list(
     CreatedAt = now_as_iso8601(),
     DerivedFrom = I(describe_file(src_path))
   ) %>% jsonlite::toJSON(pretty = TRUE, auto_unbox = TRUE) %>%
-  write(dir_output(output_affix(), ".osm.pbf.meta.json"))
+  write(paste0(dest_path, ".meta.json"))
 }
 
 download_and_prepare_osm <- function(){
