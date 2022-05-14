107f28b0950cdde0d320b28a78ebbcaf8211d81a
Adam Watkins <adam@stupidpupil.co.uk>
2022-05-14 10:57:37 +0100

    Cache TNDS downloads

---
+++ R/cache_key_for_tnds_url.R
@@ -0,0 +1,19 @@
+cache_key_for_tnds_url <- function(tnds_url){
+
+  assert_character(tnds_url, len=1, pattern="^ftp://(.+)/(.+)$")
+
+  tnds_url_components <- tnds_url %>%
+    stringr::str_match("^(.+/)([^/]*)$")
+
+  tnds_dir_listing <- RCurl::getURL(tnds_url_components[[2]])
+
+  dir_entry <- tnds_dir_listing %>%
+    stringr::str_split("\n") %>% first() %>%
+    purrr::keep(function(x){
+      x %>% stringr::str_detect(paste0(" ", tnds_url_components[[3]], "$"))
+      }) %>% first()
+
+  checkmate::assert_character(dir_entry, len=1)
+
+  openssl::sha1(dir_entry) %>% as.character()
+}
--- R/download_tnds.R
+++ R/download_tnds.R
@@ -13,13 +13,20 @@ download_tnds <- function(){
 
     src_url = paste0("ftp://",config::get()$tnds_username,":",config::get()$tnds_password,"@ftp.tnds.basemap.co.uk/", r, ".zip")
 
+    cache_key <- cache_key_for_tnds_url(src_url)
+
+    if(cache_key == cache_key_for_file(dest_path)){
+      message("Cache hit for ", dest_path)
+      next
+    }
     download.file(src_url, dest_path)
 
     jsonlite::toJSON(pretty = TRUE, auto_unbox = TRUE, list(
       SourceUrl = paste0("ftp://ftp.tnds.basemap.co.uk/",r,".zip"),
       SourceDownloadedAt = now_as_iso8601(),
       SourceLicence = "OGL-UK-3.0",
-      SourceAttribution = "Traveline"
+      SourceAttribution = "Traveline",
+      ParochialCacheKey = cache_key
     )) %>% write(paste0(dest_path, ".meta.json"))
   }
 
