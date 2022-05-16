a9799d3e5c70b75678c0522e7aff610e9ac3da61
Adam Watkins <adam@stupidpupil.co.uk>
2022-05-16 23:42:22 +0100

    Fix delete_merged_gtfs()

--- R/delete_merged_gtfs.R
+++ R/delete_merged_gtfs.R
@@ -1,3 +1,9 @@
 delete_merged_gtfs <- function(){
-  fs::delete_file(Sys.glob(dir_output("gtfs/merged.*.gtfs.zip.*")))
+  merged_gtfs <- Sys.glob(dir_output("gtfs/merged.*.gtfs.zip.*"))
+
+  if(length(merged_gtfs) > 0){
+    return(fs::file_delete(merged_gtfs))
+  }
+
+  return(character(0))
 }
