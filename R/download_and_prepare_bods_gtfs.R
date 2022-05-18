512a243ad24718ea776c54d17362edf947bb783f
Adam Watkins <adam@stupidpupil.co.uk>
2022-05-18 10:58:58 +0100

    Cache prepared BODS files

--- R/download_and_prepare_bods_gtfs.R
+++ R/download_and_prepare_bods_gtfs.R
@@ -6,7 +6,6 @@ download_and_prepare_bods_gtfs <- function(){
   output_paths <- c()
 
   for (r in bods_files) {
-    message("Downloading BODS data for ", r, "...")
     bus_url <- paste0(base_bus_url, r, '/')
     work_path <- dir_working(r, ".bods.gtfs.zip")
     output_path <- dir_output("gtfs/", r, ".bods.", output_affix(),".gtfs.zip")
@@ -16,6 +15,7 @@ download_and_prepare_bods_gtfs <- function(){
     cache_key <- cache_key_for_bods_url(bus_url)
 
     if(cache_key != cache_key_for_file(work_path)){
+      message("Downloading BODS data for ", r, "...")
       download.file(bus_url, work_path)
 
       jsonlite::toJSON(pretty = TRUE, auto_unbox = TRUE, list(
@@ -30,18 +30,30 @@ download_and_prepare_bods_gtfs <- function(){
       message("Cache hit for ", work_path)
     }
 
-    message("Preparing GTFS from \'", r, "\' BODS timetables...")
-    gtfs <- gtfstools::read_gtfs(work_path)
-    gtfs <- gtfs %>% gtfs_parochialise()
-    gtfs %>% gtfstools::write_gtfs(output_path)
+    cache_key <- openssl::sha1(paste0(
+      cache_key_for_file(work_path),
+      bounds() %>% sf::st_as_text(),
+      parochial_temporal_bounds_as_character()
+    )) %>%
+    as.character()
 
-    list(
-      CreatedAt = now_as_iso8601(),
-      DerivedFrom = I(describe_file(work_path))
-    ) %>% jsonlite::toJSON(pretty = TRUE, auto_unbox = TRUE) %>%
-    write(paste0(output_path, ".meta.json"))
+    if(cache_key != cache_key_for_file(output_path)){
+      message("Preparing GTFS from \'", r, "\' BODS timetables...")
+      gtfs <- gtfstools::read_gtfs(work_path)
+      gtfs <- gtfs %>% gtfs_parochialise()
+      gtfs %>% gtfstools::write_gtfs(output_path)
 
-    delete_merged_gtfs()
+      list(
+        CreatedAt = now_as_iso8601(),
+        DerivedFrom = I(describe_file(work_path)),
+        ParochialCacheKey = cache_key
+      ) %>% jsonlite::toJSON(pretty = TRUE, auto_unbox = TRUE) %>%
+      write(paste0(output_path, ".meta.json"))
+
+      delete_merged_gtfs()      
+    }else{
+      message("Cache hit for ", output_path)
+    }
 
   }
 
