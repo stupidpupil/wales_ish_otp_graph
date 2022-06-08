f3c8909b0a5d97d4c0ca213375538602b1f9dd53
Adam Watkins <adam@stupidpupil.co.uk>
2022-06-08 11:00:30 +0100

    Document paths_to_active_gtfs()

--- R/paths_to_active_gtfs.R
+++ R/paths_to_active_gtfs.R
@@ -1,3 +1,12 @@
+#' Get paths to GTFS zips that parochial considers 'active'
+#'
+#' @details
+#' This function returns filepaths to the GTFS zips that parochial considers 'active'
+#' and will make use of in, for example, \code{prepare_r5r_network_dat} or \code{prepare_transport_network}.
+#'
+#' Usually this is the value of \code{dir_output("gtfs/*.gtfs.zip")}.
+#' However, if a single GTFS zip begins with 'merged.' then this will be treated as the sole 'active' GTFS zip.
+#' 
 paths_to_active_gtfs <- function(){
   paths_to_all_gtfs <- Sys.glob(dir_output("gtfs/*.gtfs.zip"))
 
